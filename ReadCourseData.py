c0b683c00f53671acf4babd9b5e9599ae9e7443f
zhirezhixin <yifeil@berkeley.edu>
2019-07-31 20:29:45 -0700

    added dept

--- ReadCourseData.py
+++ ReadCourseData.py
@@ -5,9 +5,10 @@ class Course:
 	def __init__(self, crn):
 		self.crn = crn
 
-	def __init__(self, crn, course_num, section_num,\
-		campus, num_credit, course_title, days, time, cap, act, wl_cap, wl_act, \
-		instructor_name, course_duration, location, attribute = '', lab_time = []):
+	def __init__(self, UID, crn, course_num, section_num,\
+		campus, num_credit, course_title, days, startTime, endTime, cap, wl_cap, \
+		instructor_name, startDate, endDate, location, attribute = ''):
+		self.UID = UID
 		self.crn = crn
 		self.course_num = course_num
 		self.section_num = section_num
@@ -15,16 +16,46 @@ class Course:
 		self.num_credit = num_credit
 		self.course_title = course_title
 		self.days = days
-		self.time = time
+		self.startTime = startTime
+		self.endTime = endTime
 		self.cap = cap
-		self.act = act
 		self.wl_cap = wl_cap
-		self.wl_act = wl_act
 		self.instructor_name = instructor_name
-		self.course_duration = course_duration
+		self.startDate = startDate
+		self.endDate = endDate
 		self.location = location
 		self.attribute = attribute
-		self.lab_time = lab_time
+		self.lab = list()
+		self.act = list()
+		self.wl_act = list()
+
+class Instructor:
+	def __init__(self, firstName, middleName, lastName):
+		self.firstName = firstName
+		self.middleName = middleName
+		self.lastName = lastName
+		self.website = ''
+		self.email = ''
+		self.department = list()
+		self.relatedCourse = dict()
+
+class Department:
+	def __init__(self, deptName):
+		self.deptName = deptName
+		self.courses = list()
+
+class Lab:
+	def __init__(self, UID, days, startTime, endTime, \
+				startDate, endDate, instructor, location):
+		self.UID = UID
+		self.days = days
+		self.startTime = startTime
+		self.endTime = endTime
+		self.startDate = startDate
+		self.endDate = endDate
+		self.instructor = instructor
+		self.location = location
+
 
 def main():
 
@@ -37,18 +68,24 @@ def main():
 			course_raw_data = json.load(f)
 
 	clist = []
+	dlist = []
 	total_course = 0
 	for department in course_raw_data["2018 Winter De Anza"]["CourseData"]:
 		dep_course_num = 0
+		temp_dept = Department(department)
 		for c in course_raw_data["2018 Winter De Anza"]["CourseData"][department]:
 			dep_course_num += 1
-			temp_course = Course(c["CRN"], c["Crse"], c["Sec"], c["Cmp"], c["Cred"],
-							 c["Title"], c["Days"], c["Time"], c["Cap"], c["Act"], 
-							 c["WL Cap"], c["WL Act"], c["Instructor"], c["Date"],
-							 c["Location"], c["Attribute"], c["Lab Time"])
+			temp_course = Course(c["CRN"], c["CRN"], c["Crse"], c["Sec"], c["Cmp"], c["Cred"],
+							 c["Title"], c["Days"], c["Time"][:8], c["Time"][9:], c["Cap"], 
+							 c["WL Cap"], c["Instructor"], c["Date"][:5], c["Date"][6:],
+							 c["Location"], c["Attribute"])
 			clist.append(temp_course)
+			interesting_name = temp_dept.deptName + " " + temp_course.course_num + " " + temp_course.course_title 
+			if interesting_name not in temp_dept.courses:
+				temp_dept.courses.append(interesting_name)
 			total_course += 1
 		print('department', department, "has", dep_course_num, "sections.")
+		dlist.append(temp_dept)
 
 	print('total course:', total_course)
 	print('loaded course:', len(clist))
@@ -57,9 +94,11 @@ def main():
 	password = input("Password:")
 	client = MongoClient("mongodb+srv://" + username +":" + password + "@fhdatimedb-jjsjm.mongodb.net/test?retryWrites=true&w=majority")
 	db = client.get_database('yifeil_test')
-	tc = db.test1
+	tc = db.new_test_courses
+	td = db.new_test_depts
 	for course in clist:
 		temp_course = {
+			'UID' : course.UID,
 			'crn': course.crn,
 			'course_num' : course.course_num,
 			'section_num' : course.section_num,
@@ -67,19 +106,24 @@ def main():
 			'num_credit' : course.num_credit,
 			'course_title' : course.course_title,
 			'days' : course.days,
-			'time' : course.time,
+			'start_time' : course.startTime,
+			'end_time' : course.endTime,
 			'cap' : course.cap,
-			'act' : course.act,
 			'wl_cap' : course.wl_cap,
-			'wl_act' : course.wl_act,
 			'instructor_name' : course.instructor_name,
-			'course_duration' : course.course_duration,
+			'start_date' : course.startDate,
+			'end_date' : course.endDate,
 			'location' : course.location,
 			'attribute' : course.attribute,
-			'lab_time' : course.lab_time
 		}
-		tc.insert_one(temp_course)
-		print("inserted")
+		#tc.insert_one(temp_course)
+
+	for dept in dlist:
+		temp_dept = {
+			'department_name' : dept.deptName,
+			'course_list' : dept.courses
+		}
+		#td.insert_one(temp_dept)
 
 
 
